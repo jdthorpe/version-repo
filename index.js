4fc601630a861c6ae8d3845629526320bf5299d7
Jason Thorpe <jdthorpe@gmail.com>
2018-04-05 16:48:31 -0700

    general cleanup

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ var memory_repo_1 = require("./src/memory_repo");
 exports.MemoryRepo = memory_repo_1.MemoryRepo;
 var buffer_1 = require("./src/buffer");
 exports.ReadonlyBuffer = buffer_1.ReadonlyBuffer;
-//-- export { ProcessedBuffer } from "./src/processed_buffer";
 var version_resolution_1 = require("./src/version_resolution");
 exports.calculate_dependencies = version_resolution_1.calculate_dependencies;
 var transform_1 = require("./src/transform");
 exports.dTransform = transform_1.dTransform;
 exports.sTransform = transform_1.sTransform;
-//-- epxlicit export of typings works with tsc but not WebPack
-//-- export { repository, deferred_repository } from './src/typings.d';
 __export(require("./src/utils"));
 //# sourceMappingURL=index.js.map
